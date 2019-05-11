03a1ca3514e4ab7c44fb10f0de01e2adaa4c7b14
tamitani <ht4376@yahoo.co.jp>
2019-05-11 22:01:23 +0900

    新規作成

---
+++ 01.Common/cls/clsConst.vb
@@ -0,0 +1,18 @@
+﻿Public Class clsConst
+
+    ' メッセージアイコン
+    Public msbiInfo As MessageBoxIcon = MessageBoxIcon.Information
+    Public msbiErr As MessageBoxIcon = MessageBoxIcon.Error
+    Public msbiExc As MessageBoxIcon = MessageBoxIcon.Exclamation
+    Public msbiQues As MessageBoxIcon = MessageBoxIcon.Question
+    Public msbiWarn As MessageBoxIcon = MessageBoxIcon.Warning
+
+    ' メッセージボタン
+    Public msbbAbt As MessageBoxButtons = MessageBoxButtons.AbortRetryIgnore
+    Public msbbOk As MessageBoxButtons = MessageBoxButtons.OK
+    Public msbbOkCan As MessageBoxButtons = MessageBoxButtons.OKCancel
+    Public msbbOkRetCan As MessageBoxButtons = MessageBoxButtons.RetryCancel
+    Public msbbOkYesNo As MessageBoxButtons = MessageBoxButtons.YesNo
+    Public msbbOkYesNoCan As MessageBoxButtons = MessageBoxButtons.YesNoCancel
+
+End Class
