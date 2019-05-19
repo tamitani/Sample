b40d7cf2aaafa51e1b4614dea48ca5f5fdd872a2
tamitani <ht4376@yahoo.co.jp>
2019-05-19 12:49:37 +0900

    追加

---
+++ 10.SysCommon/cls/Data/clsDept.vb
@@ -0,0 +1,30 @@
+﻿Public Class clsDept
+
+    Public Property DEPTNO As String
+
+    Public Property DNAME As String
+
+    Public Property LOC As String
+
+End Class
+
+Public Class clsFrmDept
+
+    Dim tbl As New clsDept
+
+    Public Function PSelect(ByVal tbl) As Boolean
+
+        Dim blnResult As Boolean = False
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+        Return blnResult
+
+    End Function
+
+
+End Class
